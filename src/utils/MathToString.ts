b2079c80256024dd277166d3c15f7d867b545e45
serhiisezonchyk <sergey040901@gmail.com>
2023-10-10 10:51:51 +0300

    add utils

---
+++ src/utils/MathToString.ts
@@ -0,0 +1,10 @@
+export const multiply = (num1: number, num2: number): string => {
+  return `${num1}*${num2}=${num1 * num2}`;
+};
+export const sum = (num1: number, num2: number): string => {
+  return `${num1}*${num2}=${num1 + num2}`;
+};
+
+export const diff = (num1: number, num2: number): string => {
+  return `${num1}-${num2}=${num1 - num2}`;
+};
